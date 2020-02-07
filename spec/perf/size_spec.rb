803291d61afbad819c19fc3824080df53310315a
Piotr Murach <pmurach@gmail.com>
2020-02-07 22:34:41 +0000

    Change perf specs to show drastic speed improvement

--- spec/perf/size_spec.rb
+++ spec/perf/size_spec.rb
@@ -6,19 +6,19 @@ require "rspec-benchmark"
 RSpec.describe TTY::Screen, ".size" do
   include RSpec::Benchmark::Matchers
 
-  it "detectes size 180x slower than io-console" do
+  it "detectes size 15x slower than io-console" do
     expect {
       TTY::Screen.size
     }.to perform_slower_than {
       IO.console.winsize
-    }.at_most(180).times
+    }.at_most(15).times
   end
 
-  it "performs at least 2K i/s" do
-    expect { TTY::Screen.size }.to perform_at_least(2000).ips
+  it "performs at least 25K i/s" do
+    expect { TTY::Screen.size }.to perform_at_least(25_000).ips
   end
 
-  it "allocates at most 366 objects" do
-    expect { TTY::Screen.size }.to perform_allocation(366).objects
+  it "allocates at most 11 objects" do
+    expect { TTY::Screen.size }.to perform_allocation(11).objects
   end
 end
