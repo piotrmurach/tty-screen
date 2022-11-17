3bef6550be9d3d962c6386e08b0fa2c60386bda4
balakine <balakine@users.noreply.github.com>
2022-11-17 10:55:11 +0200

    Allow method

--- spec/unit/screen_spec.rb
+++ spec/unit/screen_spec.rb
@@ -56,6 +56,7 @@ RSpec.describe TTY::Screen do
   describe "#size_from_readline" do
     it "doesn't calculate size if it is run without a console" do
       require "readline" unless defined?(::Readline)
+      allow(::Readline).to receive(:method_defined?).with(:get_screen_size) { true }
       allow(screen.output).to receive(:tty?) { false }
       allow(::Readline).to receive(:get_screen_size) { [50, 201] }
       expect(screen.size_from_readline).to eq(nil)
